2eb5e324b315e2f13664e7142b6f4ed38169458a
Alexandre Ribeiro <alexandre.ribeiro@CELULA.LOCAL>
2018-10-18 11:35:48 -0300

    atualizacao1

--- loja/class/Produto.php
+++ loja/class/Produto.php
@@ -2,41 +2,69 @@
 
 class Produto {
 
-	public $id;
-	public $nome;
+	private $id;
+	private $nome;
 	private $preco;
-	public $descricao;
-	public $categoria;
-	public $usado;
+	private $descricao;
+	private $categoria;
+	private $usado;
 
+	public function getId() {
+		return $this->id;
+	}
 
-public function precoDesconto($valor = 0.1){
+	public function setId($id) {
+		$this->id = $id;
+	}
 
-if($valor > 0 && $valor <= 0.5){
+	public function getNome() {
+		return $this->nome;
+	}
 
-$this->preco -= $this->preco * $valor;
+	public function setNome($nome) {
+		$this->nome = $nome;
+	}
+
+	public function getPreco() {
+		return $this->preco;
+	}
+
+	public function setPreco($preco) {
+		$this->preco = $preco;
+	}
+
+	public function getDescricao() {
+		return $this->descricao;
+	}
+
+	public function setDescricao($descricao) {
+		$this->descricao = $descricao;
+	}
+
+	public function getCategoria() {
+		return $this->categoria;
+	}
+
+	public function setCategoria($categoria) {
+		$this->categoria = $categoria;
+	}
+
+	public function isUsado() {
+		return $this->usado;
+	}
+
+	public function setUsado($usado) {
+		$this->usado = $usado;
+	}
+
+	public function precoComDesconto($valor = 0.1) {
+
+		if ($valor > 0 && $valor <= 0.5) {
+			$this->preco -= $this->preco * $valor;
+		}
+
+		return $this->preco;
+	}
 }
-return $this->preco;
 
-}
-
-public function getPreco(){
-
-return $this->preco;
-}
-
-public function setPreco($preco){
-
-$this->preco = $preco;
-
-
-}
-
-
-
-
-
-
-
-}
 ?>
